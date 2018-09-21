e9eb2fa5b6010de03cd0b465877fa7dd2ece9b74
sapiensmagno <sapiensmagno@bol.com.br>
2018-09-21 19:19:37 -0300

    save addresses always associating them to the logged user

--- src/main/java/br/com/artemis/service/impl/AddressServiceImpl.java
+++ src/main/java/br/com/artemis/service/impl/AddressServiceImpl.java
@@ -1,14 +1,17 @@
 package br.com.artemis.service.impl;
 
-import br.com.artemis.service.AddressService;
-import br.com.artemis.domain.Address;
-import br.com.artemis.repository.AddressRepository;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import br.com.artemis.domain.Address;
+import br.com.artemis.domain.User;
+import br.com.artemis.repository.AddressRepository;
+import br.com.artemis.service.AddressService;
+import br.com.artemis.service.UserService;
 
 /**
  * Service Implementation for managing Address.
@@ -20,8 +23,17 @@ public class AddressServiceImpl implements AddressService{
     private final Logger log = LoggerFactory.getLogger(AddressServiceImpl.class);
 
     private final AddressRepository addressRepository;
-    public AddressServiceImpl(AddressRepository addressRepository) {
+    
+    private final UserService userService;
+    
+    public AddressServiceImpl(AddressRepository addressRepository, UserService userService) {
         this.addressRepository = addressRepository;
+        this.userService = userService;
+    }
+    
+    private void setAddressToLoggedUser (Address address) {
+    	User loggedUser = this.userService.getUserWithAuthorities();
+    	address.setUser(loggedUser);    	
     }
 
     /**
@@ -33,9 +45,10 @@ public class AddressServiceImpl implements AddressService{
     @Override
     public Address save(Address address) {
         log.debug("Request to save Address : {}", address);
+        this.setAddressToLoggedUser(address);
         return addressRepository.save(address);
     }
-
+    
     /**
      *  Get all the addresses.
      *
